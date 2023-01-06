8d83cdc9a09e44ab32bcbf382fdeedfade2b3765
Tom-Lincoln <tomlincoln84@gmail.com>
2023-01-06 16:09:41 +0200

    added several features

--- java1/sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
+++ java1/sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,8 +1,26 @@
 package ua.stqa.pft.sandbox;
 
 public class MyFirstProgram {
+    public static void main(String[] args) {
+        hello("world");
 
-       public static void main(String[] args) {
-              System.out.println("Hello, world!");
-              }
+        double len = 5;
+        System.out.println("Площа квадрату зі стороною " + len + " = " + area(len));
+
+        double a = 4;
+        double b = 5;
+        System.out.println("Площа прямокутника зі сторонами " + a + " і " + b + " = " + area(a, b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }
