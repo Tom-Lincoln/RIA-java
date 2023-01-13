a22362a4c2c05ef109b5160d19287010a1073135
Tom-Lincoln <tomlincoln84@gmail.com>
2023-01-13 16:28:05 +0200

    added new class and testng

--- java1/sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
+++ java1/sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,23 +4,18 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         hello("world");
 
-        double len = 5;
-        System.out.println("Площа квадрату зі стороною " + len + " = " + area(len));
+        Square s = new Square(5);
+        System.out.println("Площа квадрату зі стороною " + s.l + " = " + s.area());
 
-        double a = 4;
-        double b = 5;
-        System.out.println("Площа прямокутника зі сторонами " + a + " і " + b + " = " + area(a, b));
+        Rectangle r = new Rectangle(5, 4);
+        System.out.println("Площа прямокутника зі сторонами " + r.a + " і " + r.b + " = " + r.area());
+
+        Point ab = new Point(-1,3,6,2);
+        System.out.println("Вістань між точками X(" + ab.xa + ", " + ab.ya + ") та Y(" + ab.xb + ", " + ab.yb + ") = " + ab.point());
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area(double l) {
-        return l * l;
-    }
-
-    public static double area(double a, double b) {
-        return a * b;
-    }
 }
